970b24f3d8bfbbeda4c742abee2308461aa308cb
vitos-exe <90628482+vitos-exe@users.noreply.github.com>
2024-12-10 15:01:50 +0000

    Merged init file added

--- nvim/.config/nvim/lua/telescope-config.lua
+++
@@ -1,9 +0,0 @@
--- No need to setup Telescope
---
--- Key mappings for Telescope
-vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>Telescope find_files<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>Telescope live_grep<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fb', [[<cmd>Telescope buffers<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fh', [[<cmd>Telescope help_tags<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fr', [[<cmd>Telescope lsp_references<CR>]], { noremap = true, silent = true })
-
