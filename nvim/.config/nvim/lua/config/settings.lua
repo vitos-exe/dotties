850e9ccab8bb1c2c5489494d812e6219096eb33c
Vitalii Chernysh <vivich4omega@gmail.com>
2024-12-24 15:52:09 +0100

    Packer changed with lazy.nvim

---
+++ nvim/.config/nvim/lua/config/settings.lua
@@ -0,0 +1,19 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.foldmethod = 'marker'
+vim.opt.foldlevel = 0
+
+local opts = { noremap = true, silent = true }
+
+vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>cb', ':bd<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader><Tab>', ':bnext<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader><S-Tab>', ':bprev<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>R', ':luafile $MYVIMRC<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>sh', ':sp<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>sv', ':vsp<CR>', opts)
+
