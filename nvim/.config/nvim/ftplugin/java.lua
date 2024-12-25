4bea6594a4e00abf04c792c4f1aad920e837445e
Vitalii Chernysh <vivich4omega@gmail.com>
2024-12-25 23:04:51 +0100

    Some fixes

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -62,8 +62,9 @@ local config = {
 	init_options = {
 		bundles = {},
 	},
-	capabilities = require("cmp_nvim_lsp").default_capabilities()
+	capabilities = require('blink.cmp').get_lsp_capabilities()
 }
 -- This starts a new client & server,
 -- or attaches to an existing client & server depending on the `root_dir`.
 require("jdtls").start_or_attach(config)
+
