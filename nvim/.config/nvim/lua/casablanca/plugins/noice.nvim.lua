1190ed64cf2a9650e2306f37add8fc70179cea78
Vitalii Chernysh <vivich4omega@gmail.com>
2025-01-03 16:42:16 +0100

    noice.nvim added; tmux config updated

--- nvim/.config/nvim/lua/casablanca/plugins/noice.nvim.lua
+++ nvim/.config/nvim/lua/casablanca/plugins/noice.nvim.lua
@@ -1,10 +1,17 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+		presets = {
+			bottom_search = true,
+			command_palette = true,
+			long_message_to_split = true,
+			inc_rename = false,
+			lsp_doc_border = false,
+		}
+	},
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 		"rcarriga/nvim-notify",
 	}
 }
-
