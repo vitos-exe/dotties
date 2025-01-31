4b5d099498e5467ec61518cf9978bded4c5777c9
Vitalii Chernysh <vivich4omega@gmail.com>
2025-01-31 13:39:37 +0100

    snacks and trouble configs updated

--- nvim/.config/nvim/lua/casablanca/plugins/snacks.nvim.lua
+++ nvim/.config/nvim/lua/casablanca/plugins/snacks.nvim.lua
@@ -5,21 +5,15 @@ return {
 		dashboard = { enabled = true },
 		indent = { enabled = true },
 		scroll = { enabled = true },
-		words = { enabled = true },
 	},
 	keys = {
-		{ "<leader>.",   function() Snacks.scratch() end,                 desc = "Toggle Scratch Buffer" },
-		{ "<leader>S",   function() Snacks.scratch.select() end,          desc = "Select Scratch Buffer" },
 		{ "<leader>bd",  function() Snacks.bufdelete() end,               desc = "Delete Buffer" },
 		{ "<leader>bdA", function() Snacks.bufdelete.all() end,           desc = "Delete All Buffers" },
 		{ "<leader>cR",  function() Snacks.rename.rename_file() end,      desc = "Rename File" },
-		{ "<leader>gB",  function() Snacks.gitbrowse() end,               desc = "Git Browse",                  mode = { "n", "v" } },
 		{ "<leader>gb",  function() Snacks.git.blame_line() end,          desc = "Git Blame Line" },
 		{ "<leader>gf",  function() Snacks.lazygit.log_file() end,        desc = "Lazygit Current File History" },
 		{ "<leader>gg",  function() Snacks.lazygit() end,                 desc = "Lazygit" },
 		{ "<leader>gl",  function() Snacks.lazygit.log() end,             desc = "Lazygit Log (cwd)" },
-		{ "]]",          function() Snacks.words.jump(vim.v.count1) end,  desc = "Next Reference",              mode = { "n", "t" } },
-		{ "[[",          function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference",              mode = { "n", "t" } },
 	},
 	init = function()
 		vim.api.nvim_create_autocmd("User", {
