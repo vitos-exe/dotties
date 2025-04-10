51b34ea07dc25c29da238032e25788ec863d39ba
Vitalii Chernysh <vivich4omega@gmail.com>
2025-04-10 15:26:58 +0200

    Function for setting difftastic context defined

---
+++ fish/.config/fish/custom/difftastic.fish
@@ -0,0 +1,6 @@
+function set_difft_context
+    set -x -g DFT_CONTEXT $argv[1]
+end
+
+set_difft_context 10
+
--- fish/.config/fish/custom/env.fish
+++ fish/.config/fish/custom/env.fish
@@ -10,8 +10,5 @@ export HOMEBREW_NO_ENV_HINTS=1
 # Disable light colors for difftastic
 export DFT_BACKGROUND=light
 
-# difftastic context window
-#export DFT_CONTEXT=1000
-
 export TESTCONTAINERS_RYUK_DISABLED=true
 
