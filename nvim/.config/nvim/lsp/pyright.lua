377c652cab94daa207980f895f26f5e849e10ec5
Vitalii Chernysh <vivich4omega@gmail.com>
2025-06-03 11:52:38 +0200

    pyright config improved & project picker added

--- nvim/.config/nvim/lsp/pyright.lua
+++ nvim/.config/nvim/lsp/pyright.lua
@@ -1,3 +1,18 @@
+local function set_python_path(path)
+  local clients = vim.lsp.get_clients {
+    bufnr = vim.api.nvim_get_current_buf(),
+    name = 'pyright',
+  }
+  for _, client in ipairs(clients) do
+    if client.settings then
+      client.settings.python = vim.tbl_deep_extend('force', client.settings.python, { pythonPath = path })
+    else
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = path } })
+    end
+    client.notify('workspace/didChangeConfiguration', { settings = nil })
+  end
+end
+
 return {
   cmd = { 'pyright-langserver', '--stdio' },
   filetypes = { 'python' },
