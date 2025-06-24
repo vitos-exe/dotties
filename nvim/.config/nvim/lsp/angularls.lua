f47b390024fe3e6e0b0c97273c2d5665df1fa097
Vitalii Chernysh <vivich4omega@gmail.com>
2025-06-24 19:21:20 +0200

    Angular language server and jdtls configuration changed

--- nvim/.config/nvim/lsp/angularls.lua
+++ nvim/.config/nvim/lsp/angularls.lua
@@ -5,7 +5,9 @@ return {
 		"--tsProbeLocations",
 		"/usr/local/lib/node_modules/typescript/lib",
 		"--ngProbeLocations",
-		"/usr/local/lib/node_modules/@angular/language-server/bin"
+		"/usr/local/lib/node_modules/@angular/language-server/bin",
+		"--forceStrictTemplates",
+		"--includeCompletionsWithSnippetText"
 	},
 	filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx", "htmlangular" },
 	root_markers = { "angular.json", "nx.json" }
