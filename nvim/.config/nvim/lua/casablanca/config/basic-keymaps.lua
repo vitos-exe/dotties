4364e9bd075fd57f1812670b3ac07d40da43a100
Vitalii Chernysh <vivich4omega@gmail.com>
2025-11-07 11:37:12 +0100

    tmux & nvim: vim-tmux-navigator pluging added

--- nvim/.config/nvim/lua/casablanca/config/basic-keymaps.lua
+++ nvim/.config/nvim/lua/casablanca/config/basic-keymaps.lua
@@ -1,10 +1,5 @@
 local opts = { noremap = true, silent = true }
 
--- Key mappings for easier window navigation
-vim.keymap.set('n', '<C-H>', '<C-W>h', opts)
-vim.keymap.set('n', '<C-J>', '<C-W>j', opts)
-vim.keymap.set('n', '<C-K>', '<C-W>k', opts)
-vim.keymap.set('n', '<C-L>', '<C-W>l', opts)
 -- Splits
 vim.keymap.set('n', '<C-S>', '<C-W>s', opts)
 vim.keymap.set('n', '<C-V>', '<C-W>v', opts)
